fed926c4ee6f9204379afe13c1baeebc9891c061
jessc1 <mmariajessica@gmail.com>
2020-06-03 00:02:03 -0300

    User registration and user profiles

--- account/urls.py
+++ account/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, include
 from django.contrib.auth import views as auth_views
 from . import views
 
@@ -7,6 +7,8 @@ urlpatterns=[
     #previous login vieew
     #path('login/', views.user_login, name='login'),
     path('', views.dashboard, name= 'dashboard'),
+    path('', include('django.contrib.auth.urls')),
+    path('register/', views.register, name='register'),
     path('login/', auth_views.LoginView.as_view(), name = 'login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('password_change/', auth_views.PasswordChangeView.as_view(),
