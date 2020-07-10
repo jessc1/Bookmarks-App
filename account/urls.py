119a26c589fd407d0eb68706fd252d0b0b4e4c9a
jessc1 <mmariajessica@gmail.com>
2020-07-09 23:55:00 -0300

    using redis to store item counts and image ranking

--- account/urls.py
+++ account/urls.py
@@ -32,4 +32,5 @@ urlpatterns=[
     path('edit/', views.edit, name = 'edit'),
     path('users/', views.user_list, name='user_list'),
     path('users/<username>/',views.user_detail, name='user_detail'),
+    path('user/follow/', views.user_follow, name='user_follow'),
 ]
--- account/views.py
+++ account/views.py
@@ -1,4 +1,5 @@
 from django.http import HttpResponse
+from actions.models import Action
 from django.shortcuts import render
 from django.contrib.auth import authenticate, login
 from .forms import LoginForm, UserRegistrationForm, UserEditForm, ProfileEditForm
@@ -7,6 +8,12 @@ from .models import Profile
 from django.contrib import messages
 from django.shortcuts import get_object_or_404
 from django.contrib.auth.models import User
+from django.http import JsonResponse
+from django.views.decorators.http import require_POST
+from common.decorators import ajax_required
+from .models import Contact
+from actions.utils import create_action
+
 
 
 
@@ -59,33 +66,45 @@ def edit(request):
                   {'user_form': user_form,
                    'profile_form': profile_form})
 
-        
+@login_required
 def dashboard(request):
+    # Display all actions by default
+    actions = Action.objects.exclude(user=request.user)
+    following_ids = request.user.following.values_list('id',
+                                                       flat=True)
+    if following_ids:
+        # If user is following others, retrieve only their actions
+        actions = actions.filter(user_id__in=following_ids)
+    actions = actions.select_related('user', 'user__profile')\
+                     .prefetch_related('target')[:10]
+
     return render(request,
                   'account/dashboard.html',
-                  {'section': 'dashboard'})
-
+                  {'section': 'dashboard',
+                   'actions': actions})
 def register(request):
     if request.method == 'POST':
         user_form = UserRegistrationForm(request.POST)
         if user_form.is_valid():
-            # Create a new user object bus avoid saving it yet
-            new_user = user_form.save(commit = False)
+            # Create a new user object but avoid saving it yet
+            new_user = user_form.save(commit=False)
             # Set the chosen password
             new_user.set_password(
                 user_form.cleaned_data['password'])
             # Save the User object
             new_user.save()
-            Profile.objects.create(user = new_user)
+            # Create the user profile
+            Profile.objects.create(user=new_user)
+            create_action(new_user, 'has created an account')
             return render(request,
                           'account/register_done.html',
                           {'new_user': new_user})
-
     else:
         user_form = UserRegistrationForm()
     return render(request,
                   'account/register.html',
-                  {'user_form':user_form})
+                  {'user_form': user_form})
+
 
 @login_required
 def user_list(request):
@@ -104,5 +123,29 @@ def user_detail(request, username):
                    'account/user/detail.html',
                    {'section': 'people',
                    'user': user})
+
+@ajax_required
+@ajax_required
+@require_POST
+@login_required
+def user_follow(request):
+    user_id = request.POST.get('id')
+    action = request.POST.get('action')
+    if user_id and action:
+        try:
+            user = User.objects.get(id=user_id)
+            if action == 'follow':
+                Contact.objects.get_or_create(user_from=request.user,
+                                              user_to=user)
+                create_action(request.user, 'is following', user)
+            else:
+                Contact.objects.filter(user_from=request.user,
+                                       user_to=user).delete()
+            return JsonResponse({'status':'ok'})
+        except User.DoesNotExist:
+            return JsonResponse({'status':'error'})
+    return JsonResponse({'status':'error'})
+
+
 
 
